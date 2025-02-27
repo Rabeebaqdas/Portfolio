7d293ffafb8686c95e3810e2d170e41851693097
Rabeeb Aqdas Jilani <63879512+Rabeebaqdas@users.noreply.github.com>
2025-02-27 12:13:41 +0500

    Update ResumeNew.js

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -39,7 +39,7 @@ function ResumeNew() {
         <Row className="resume">
           <Document
             file={pdf}
-            className="d-flex justify-content-center"
+            className="d-flex gap-3 flex-column justify-content-center"
             onLoadSuccess={onLoadSuccess}
           >
             {/* Render each page dynamically based on the number of pages */}

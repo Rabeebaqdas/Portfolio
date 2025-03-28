d9ff35e2fa2d9b593c0020d0a557ff9975f193cd
Rabeeb Aqdas Jilani <63879512+Rabeebaqdas@users.noreply.github.com>
2025-03-28 14:29:59 +0500

    Update AboutCard.js

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -10,13 +10,13 @@ function AboutCard() {
           <p style={{ textAlign: "justify" }}>
             Hi, I am <span className="purple">Rabeeb Aqdus </span>
             from <span className="purple"> Karachi, Pakistan.</span>
-            <br /> I am a Software Engineer, Smart Contract Engineer and MERN Stack Developer
+            <br /> I am a passionate Software Engineer, Smart Contract Developer, and MERN Stack Developer.
             <br />
-            Additionally, I am currently employed as a Blockchain Engineer at
+            Currently, I am working as a Smart Contract Engineer at
             Decrypted Labs.
             <br />
             <br />
-            Apart from coding, some other activities that I love to do!
+            Apart from coding, some activities I enjoy include:
           </p>
           <ul>
             <li className="about-activity">
@@ -30,7 +30,7 @@ function AboutCard() {
           <p style={{ color: "rgb(155 126 172)" }}>
             "Strive to build things that make a difference!"{" "}
           </p>
-          <footer className="blockquote-footer">Rabeeb</footer>
+          <footer className="blockquote-footer">Rabeeb Aqdus</footer>
         </blockquote>
       </Card.Body>
     </Card>
